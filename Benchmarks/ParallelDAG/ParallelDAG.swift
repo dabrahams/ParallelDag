ac07516b28f528a46d3718d2796c6420266de42f
Dave Abrahams <dabrahams@adobe.com>
2024-09-06 16:30:03 -0700

    Try using a mutex instead of an actor.

--- Benchmarks/ParallelDAG/ParallelDAG.swift
+++ Benchmarks/ParallelDAG/ParallelDAG.swift
@@ -1,3 +1,108 @@
+#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+import Darwin
+#elseif os(Windows)
+import ucrt
+import WinSDK
+#else
+import Glibc
+#endif
+
+// SHAMELESSLY LIFTED FROM SwiftNIO
+// https://github.com/apple/swift-nio/blob/0467886d0b21599fdf011cd9a89c5c593dd650a7/Sources/NIOConcurrencyHelpers/lock.swift#L30-L53
+
+/// A threading lock based on `libpthread` instead of `libdispatch`.
+///
+/// This object provides a lock on top of a single `pthread_mutex_t`. This kind
+/// of lock is safe to use with `libpthread`-based threading models, such as the
+/// one used by NIO. On Windows, the lock is based on the substantially similar
+/// `SRWLOCK` type.
+public final class Lock: Sendable {
+#if os(Windows)
+    fileprivate let mutex: UnsafeMutablePointer<SRWLOCK> =
+        UnsafeMutablePointer.allocate(capacity: 1)
+#else
+    fileprivate let mutex: UnsafeMutablePointer<pthread_mutex_t> =
+        UnsafeMutablePointer.allocate(capacity: 1)
+#endif
+
+    /// Create a new lock.
+    public init() {
+#if os(Windows)
+        InitializeSRWLock(self.mutex)
+#else
+        var attr = pthread_mutexattr_t()
+        pthread_mutexattr_init(&attr)
+//        debugOnly {
+//            pthread_mutexattr_settype(&attr, .init(PTHREAD_MUTEX_ERRORCHECK))
+//        }
+
+        let err = pthread_mutex_init(self.mutex, &attr)
+        precondition(err == 0, "\(#function) failed in pthread_mutex with error \(err)")
+#endif
+    }
+
+    deinit {
+#if os(Windows)
+        // SRWLOCK does not need to be free'd
+#else
+        let err = pthread_mutex_destroy(self.mutex)
+        precondition(err == 0, "\(#function) failed in pthread_mutex with error \(err)")
+#endif
+        mutex.deallocate()
+    }
+
+    /// Acquire the lock.
+    ///
+    /// Whenever possible, consider using `withLock` instead of this method and
+    /// `unlock`, to simplify lock handling.
+    public func lock() {
+#if os(Windows)
+        AcquireSRWLockExclusive(self.mutex)
+#else
+        let err = pthread_mutex_lock(self.mutex)
+        precondition(err == 0, "\(#function) failed in pthread_mutex with error \(err)")
+#endif
+    }
+
+    /// Release the lock.
+    ///
+    /// Whenver possible, consider using `withLock` instead of this method and
+    /// `lock`, to simplify lock handling.
+    public func unlock() {
+#if os(Windows)
+        ReleaseSRWLockExclusive(self.mutex)
+#else
+        let err = pthread_mutex_unlock(self.mutex)
+        precondition(err == 0, "\(#function) failed in pthread_mutex with error \(err)")
+#endif
+    }
+}
+
+extension Lock {
+    /// Acquire the lock for the duration of the given block.
+    ///
+    /// This convenience method should be preferred to `lock` and `unlock` in
+    /// most situations, as it ensures that the lock will be released regardless
+    /// of how `body` exits.
+    ///
+    /// - Parameter body: The block to execute while holding the lock.
+    /// - Returns: The value returned by the block.
+    @inlinable
+    public func withLock<T>(_ body: () throws -> T) rethrows -> T {
+        self.lock()
+        defer {
+            self.unlock()
+        }
+        return try body()
+    }
+
+    // specialise Void return (for performance)
+    @inlinable
+    public func withLockVoid(_ body: () throws -> Void) rethrows -> Void {
+        try self.withLock(body)
+    }
+}
+
 import Benchmark
 import Foundation
 
@@ -71,7 +176,6 @@ class Op: Operation {
   }
 }
 
-
 actor Operations {
   private var r: [Int: Op] = [:]
   private let q = OperationQueue()
@@ -101,8 +205,6 @@ actor Operations {
     return r1
   }
 }
-
-
 
 actor Cache {
     var r: [Int: Int] = [:]
@@ -134,6 +236,68 @@ func compute(_ input: [Int]) async -> [Int: Int] {
     }
 }
 
+
+final class MutexCache: Sendable {
+
+  private let mutex = Lock()
+
+
+  private var _r: [Int: Int] = [:]
+
+  var r: [Int: Int] {
+    _read {
+      mutex.lock()
+      yield _r
+      mutex.unlock()
+    }
+    _modify {
+      mutex.lock()
+      yield &_r
+      mutex.unlock()
+    }
+  }
+
+  func update(key: Int, with value: Int) {
+    self.r[key] = value
+  }
+}
+
+func mutexCompute(_ input: [Int]) async -> [Int: Int] {
+    let mutexCache = MutexCache()
+
+    @Sendable
+    @discardableResult
+    func fib(_ x: Int, cache: MutexCache) -> Int {
+        if let y = cache.r[x] { return y }
+        let y = x < 2 ? 1 : fib(x - 1, cache: cache) + fib(x - 2, cache: cache)
+        cache.r[x] = y
+        return y
+    }
+
+    await withTaskGroup(of: Void.self) { group in
+        for z in input {
+            group.addTask {
+                fib(z, cache: mutexCache)
+            }
+        }
+        await group.waitForAll()
+    }
+    return mutexCache.r
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let benchmarks = {
 
   #if false
@@ -160,9 +324,15 @@ let benchmarks = {
     }
   }
 
-  Benchmark("Jaleel") { benchmark in
+  Benchmark("TaskGroupWithActor") { benchmark in
     for _ in benchmark.scaledIterations {
       blackHole(await compute([2, 10, 15, 6, 20, 91, 4, 5]))
+    }
+  }
+
+  Benchmark("TaskGroupWithMutex") { benchmark in
+    for _ in benchmark.scaledIterations {
+      blackHole(await mutexCompute([2, 10, 15, 6, 20, 91, 4, 5]))
     }
   }
 }
